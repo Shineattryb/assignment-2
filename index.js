b67ca0766dfb81280efbd869a27728833fe55c6b
shine_52 <156677365+Shineattryb@users.noreply.github.com>
2024-01-30 12:23:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context';
+var mysql = require('mysql2/promise');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+var express=require('express');
+var cors= require('cors');
+const app=express();
+const port =8000;
+app.use(cors("*"));
+const pool = mysql.createPool({
+    host: 'monorail.proxy.rlwy.net',
+    user: 'root',
+    password: '3H1AA-3f6eE3FB3E2-A3bBaDDGe6DA1b',
+    database: 'railway',
+    port:'36742',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+    
+});
+pool.getConnection()
+    .then(connection => {
+        console.log('Connected to MySQL');
+        // connection.release();
+    })
+    .catch(error => {
+        console.error('Error connecting to MySQL:', error);
+    });
+app.get('/', (req, res) => {
+        res.send('Welcome to API!');
+});
 
-    <App />
-   
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+app.get('/api/cards',async(req,res)=>{
+    try {
+        const page = req.query.page ? parseInt(req.query.page) : 1;
+        const limit = req.query.limit ? parseInt(req.query.limit) : 20;
+        const offset = (page - 1) * limit;
+        const [rows] = await pool.query('SELECT * FROM Card');
+     
+        res.json(rows);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+
+
+});
+
+app.listen(port,()=>{
+    console.log('server is running on port');
+})
